069850a191176dcab070aabc6a8786c3fdc91b38
Jesus Avina <jesus.avina@rooi.tech>
2025-11-08 12:00:07 -0600

    singleton created

--- src/app/lib/supabaseClient.js
+++ src/app/lib/supabaseClient.js
@@ -10,3 +10,15 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // Export the Supabase client for use in other part of the application
 
 export default supabase;
+
+
+// ¿Por qué esto es un Singleton?
+// En el contexto de los módulos de JavaScript (como en Next.js), cualquier módulo (un archivo) se evalúa y se ejecuta una sola vez cuando se importa por primera vez en la aplicación.
+
+// createClient() se llama solo una vez cuando el archivo supabaseClient.js es cargado.
+
+// La instancia resultante se almacena en la variable supabase.
+
+// Cualquier otro archivo que haga import supabase from 'lib/supabaseClient' recibirá una referencia a esa misma instancia ya creada, no creará una nueva.
+
+// ¡Felicidades, has implementado tu primer patrón (Singleton) de forma moderna y eficiente!
